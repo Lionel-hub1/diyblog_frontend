6f22b4a54c460c3c230d3ae72f0a104e6ec0055d
lionel <ndabagajeanlionel@gmail.com>
2024-06-27 12:49:04 +0200

    refactor: Update UI styles for Blogs and Services pages

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,17 +1,19 @@
 import { Link } from "react-router-dom";
 
 const Services = () => {
-    return (<div className="h-[100vh] flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold mb-4">
-            Oops! This page is still under construction.
-        </h1>
-        <p className="text-lg mb-8 text-gray-600">
-            It looks like Lionel hasn&apos;t finished designing this page yet.
-        </p>
-        <Link to="/contact" className="bg-[#FFA559] font-black text-white px-4 py-2 rounded-md">
-            Contact Lionel
-        </Link>
-    </div>);
+    return (
+        <div className="h-[100vh] text-center flex flex-col items-center justify-center">
+            <h1 className="text-4xl font-bold mb-4">
+                Oops! This page is still under construction.
+            </h1>
+            <p className="text-lg mb-8 text-gray-600">
+                It looks like Lionel hasn&apos;t finished designing this page yet.
+            </p>
+            <Link to="/contact" className="bg-[#FFA559] font-black text-white px-4 py-2 rounded-md">
+                Contact Lionel
+            </Link>
+        </div>
+    );
 }
 
 export default Services;
